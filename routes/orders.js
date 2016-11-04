a7e4186d44833d8a9e0731cfa44fe5d8d8eec0ad
strdr4605 <str.dr4605@gmail.com>
2016-11-04 15:44:13 +0200

    add filtered Orders by distance

--- routes/orders.js
+++ routes/orders.js
@@ -18,6 +18,42 @@ router.get('/getAllOrders', (req, res) => {
   })
 })
 
+let rad = function(x) {
+  return x * Math.PI / 180;
+};
+
+let getDistance = function(lat1, long1, lat2, long2) {
+  let R = 6378137; // Earth’s mean radius in meter
+  let dLat = rad(lat2 - lat1);
+  let dLong = rad(long2 - long1);
+  let a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(rad(lat1)) * Math.cos(rad(lat2)) *
+    Math.sin(dLong / 2) * Math.sin(dLong / 2);
+  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  let d = R * c;
+  return d; // returns the distance in meter
+};
+
+router.get('/getNearOrders', (req, res) => {
+  let lat = req.query.lat
+  let long = req.query.long
+  let distance = req.query.distance
+
+  let inRange = function(order) {
+    return getDistance(lat, long, order.start.latitude, order.start.longitude) <= distance
+  }
+
+  Order.find({}, (err, doc) => {
+    if (err) {
+      res.json(err)
+    } else {
+      orders = doc
+      filteredOrders = orders.filter(inRange)
+      res.json(filteredOrders)
+    }
+  })
+})
+
 // End GET
 
 // Start POST
